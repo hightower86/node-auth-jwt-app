36382c6016430c16ac290600c3500be3986e7f37
Adel <htwr86@gmail.com>
2019-11-06 11:28:40 +0300

    checking existing email

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 
 router.get('/register', (req, res) => res.send('Hello'));
 
 router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
   // LETS VALIDATE THE DATA BEFORE WI HANE A USER
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+  // Checking if a user email exist in database
+  const emailExists = await User.findOne({ email: email });
+  if (emailExists)
+    return res.status(400).send('This email already exist in database');
 
-  const { name, email, password } = req.body;
+  // CTEATE A NEW USER
   const user = new User({
     name,
     email,
--- validation.js
+++ validation.js
@@ -18,4 +18,19 @@ const registerValidation = data => {
   return schemaRegister.validate(data);
 };
 
+const schemaLogin = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+});
+
+const loginValidation = data => {
+  return schemaLogin.validate(data);
+};
+
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
